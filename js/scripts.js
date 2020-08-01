534bcb981f1022bfcf4325c1402ca5a999eca0c5
rolteame <r.s.adebiyi@gmail.com>
2020-08-01 15:07:53 +0100

    final

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+let formatMoney = (value) => {
+    VALUE = Math.ceil(value * 100) / 100;
+    value = value.toFixed(2);
+    return "$ " + value;
+}
+
+let formatSplit = (value) => {
+    if (value === '1') {
+        return value + " person";
+    }else {
+        return value + " people";
+    }
+}
+
+update = () => {
+    let bill = Number(document.getElementById('yourBill').value);
+    let tipPercent = document.getElementById('tipInput').value;
+    let split = document.getElementById('splitInput').value;
+
+    let tipValue = bill * (tipPercent/100);
+
+    let tipEach = tipValue / split;
+
+    let newBillEach = (bill * tipValue) / split
+
+    document.getElementById('tipPercent').innerHTML = tipPercent + "%";
+
+    document.getElementById('tipValue').innerHTML = formatMoney(tipValue);
+
+    document.getElementById('totalWithTip').innerText = formatMoney(bill + tipValue);
+
+    document.getElementById('splitValue').innerHTML = formatSplit(split);
+
+    document.getElementById('billEach').innerHTML = formatMoney(newBillEach);
+
+    document.getElementById('tipEach').innerHTML = formatMoney(tipEach);
+}
+let container = document.getElementById('container');
+container.addEventListener('input', update);
